a1e118f8c7005248d47fa36bf57db8819457edd7
Mukul <mukul1singh23@gmail.com>
2023-01-07 15:55:26 +0530

    print decresing

--- lec1/print_decresing_5.java
+++ lec1/print_decresing_5.java
@@ -1,5 +1,5 @@
 package lec1;
-public class print_5 {
+public class print_decresing_5 {
     public static void main(String args[])
     {
         int n=5;
@@ -7,12 +7,13 @@ public class print_5 {
     }
     public static void prn(int n)
     {
-        System.out.println(n);
-        prn(n-1);
         if(n==0)
         {   
             return ;
         }
+        System.out.println(n);
+        prn(n-1);
+        
         
     
 }
