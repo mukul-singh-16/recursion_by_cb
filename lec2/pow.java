107d58e7a887d543f3900dce3aee343e03f01f1a
Mukul <mukul1singh23@gmail.com>
2023-01-07 15:49:42 +0530

    power function

---
+++ lec2/pow.java
@@ -0,0 +1,15 @@
+package lec2;
+
+public class pow {
+    public static void main(String[] args) {
+        int a=2;
+        int b=30;
+        System.out.println(pow(a,b));
+    }
+    public static int pow(int a ,int b)
+    {
+        if(b==0)
+            return 1;
+        return pow(a,b-1) *a;
+    }
+}
