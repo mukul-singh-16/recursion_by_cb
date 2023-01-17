99661520ef49ed92ddf333f2748208238d1d8c55
Mukul <mukul1singh23@gmail.com>
2023-01-17 21:50:00 +0530

    cointos count

---
+++ lec4/coinTosCount.java
@@ -0,0 +1,18 @@
+package lec4;
+
+public class coinTosCount {
+    public static void main(String[] args) {
+        int n=3;
+        System.out.println(cointos(n, ""));
+    }
+    public static int cointos(int n,String ans)
+    {
+        if(n==0)
+        {
+            System.out.println(ans);
+            return 1;
+        }
+        return cointos(n-1, ans+'T') + cointos(n-1, ans+'H');
+    }
+    
+}
